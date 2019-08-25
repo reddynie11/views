979cd76f75fe348c9d924039ea4732bad90b0daf
Narasimha Reddy <reddy.nie11@gmail.com>
2019-08-25 15:04:51 +0530

    'files

---
+++ files_rootComponent/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div> 
+    <h1>{{title}}</h1>
+    <p>Please enter your name below</p>
+    <input type="text" ref="name">
+    <button v-on:click="changeName" >Submit</button>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title : 'Root Component',
+      name:''
+    }
+  },
+  methods:{
+    changeName(){
+      this.name = this.$refs.name.value;
+      this.title = 'Welcome '+ this.name+' !!!';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
